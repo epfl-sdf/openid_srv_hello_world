0d857d75c9b2ee6b89f9966fe853da0d2f5d17c8
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-16 15:59:50 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -5,3 +5,7 @@ curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
 sudo apt-get update
 
 sudo apt-get install gluu-server-3.0.2
+
+sudo service gluu-server-3.0.2 start
+
+sudo service gluu-server-3.0.2 login
