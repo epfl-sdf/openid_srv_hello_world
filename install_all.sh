fdd3211497f00ccdb426604c55d337007db53a41
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-16 15:53:54 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,3 +1,5 @@
 echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee /etc/apt/sources.list.d/gluu-repo.list
 
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
+
+sudo apt-get update
