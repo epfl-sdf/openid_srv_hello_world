53754eae270e4625de8790a234e5ea893f451504
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-16 15:54:32 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -3,3 +3,5 @@ echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee /etc/apt/sources
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
 
 sudo apt-get update
+
+sudo apt-get install gluu-server-3.0.2
