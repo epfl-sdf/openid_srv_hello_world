f2e18d1836b9088d11a525b9dc285a90ebcc88ad
jeniffer9 <jeniffer.limagraf@epfl.ch>
2017-08-21 13:16:38 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#Installation du serveur
+#zf170821.1316
+
 echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee /etc/apt/sources.list.d/gluu-repo.list
 
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
@@ -10,8 +14,3 @@ sudo service gluu-server-3.0.2 start
 
 sudo service gluu-server-3.0.2 login
 
-cd /install/community-edition-setup
-
-# look if we can simplify y y y y n n n n prompts
-
-./setup.py
