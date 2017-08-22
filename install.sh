a5d44ca918778fcfdb19ada73daf0a622af5e11a
Christian Zufferey <christian@zufferey.com>
2017-08-22 16:51:49 +0200

    - ajouté le -y à la commande apt-get install

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #Installation du serveur
-#zf170821.1316
+#zf170822.1650
 
 echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee /etc/apt/sources.list.d/gluu-repo.list
 
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
 
 sudo apt-get update
-
-sudo apt-get install gluu-server-3.0.2
+sudo apt-get -y install gluu-server-3.0.2
 
